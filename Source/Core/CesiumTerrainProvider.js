d6f6ae4e4b4ed0cc2c79303a339d1a939b12fc8f
Keat Tang <keat@propelleraero.com.au>
2015-10-26 10:57:16 +1100

    Revert "fixed proxying bug"
    This reverts commit 85478b23b23e32c7489d62f4dbafb7758691ed82.

--- Source/Core/CesiumTerrainProvider.js
+++ Source/Core/CesiumTerrainProvider.js
@@ -190,7 +190,7 @@ define([
                 return;
             }
 
-            var baseUri = new Uri(that._url);
+            var baseUri = new Uri(metadataUrl);
 
             that._tileUrlTemplates = data.tiles;
             for (var i = 0; i < that._tileUrlTemplates.length; ++i) {
