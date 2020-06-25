976c9cf727c5e46d343e04b552e54f6eb1837822
katrin <katrin.schmid@propelleraero.com.au>
2020-06-25 12:23:17 +1000

    fix variables

--- index.cjs
+++ index.cjs
@@ -6,9 +6,8 @@ var path = require("path");
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
 if (
   process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "deploy-staging" ||
-  process.env.NODE_ENV === "deploy-production" ||
-  process.env.NODE_ENV === "deploy-dev"
+  process.env.NODE_ENV === "staging" ||
+  process.env.NODE_ENV === "dev"
 ) {
   module.exports = require(path.join(__dirname, "Build/Cesium/Cesium"));
   return;
