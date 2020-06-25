ae1f2560bb1835598ba7d9a76c4e112a0bd158ef
katSchmid <65633379+katSchmid@users.noreply.github.com>
2020-06-25 12:27:09 +1000

    fix deploy (#10)
    * fix deploy
    
    * version up
    
    * fix variables

--- index.cjs
+++ index.cjs
@@ -4,7 +4,11 @@
 var path = require("path");
 
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging" ||
+  process.env.NODE_ENV === "dev"
+) {
   module.exports = require(path.join(__dirname, "Build/Cesium/Cesium"));
   return;
 }
