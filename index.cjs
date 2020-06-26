f90e4a872fe2b82847bff4e7bc8816638bc941d5
Chris Cooper <chris@propelleraero.com>
2020-06-26 13:39:35 +1000

    signpost recent tweak

--- index.cjs
+++ index.cjs
@@ -4,6 +4,7 @@
 var path = require("path");
 
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
+// PROPELLER HACK: Use minified code in all deployed lambda environments for compactness
 if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging" ||
