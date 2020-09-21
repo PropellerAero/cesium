e1e0605a5892ca3a9133b244209355a5c0100519
Chris Cooper <chris@propelleraero.com>
2020-09-21 15:19:02 +1000

    All the Propeller mods in a single commit
    coops fail
    
    rotate codeship tokens (#14)
    
    * rotate codeship tokens
    
    * this?
    
    try to push tag
    
    oops

--- index.cjs
+++ index.cjs
@@ -4,7 +4,12 @@
 var path = require("path");
 
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
-if (process.env.NODE_ENV === "production") {
+// PROPELLER HACK: Use minified code in all deployed lambda environments for compactness
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging" ||
+  process.env.NODE_ENV === "dev"
+) {
   module.exports = require(path.join(__dirname, "Build/Cesium/Cesium"));
   return;
 }
