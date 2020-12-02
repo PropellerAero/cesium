3ee3056bf06dc32f460fbcfa7686b963acd0a626
Chris Cooper <chris@propelleraero.com>
2020-12-02 14:20:50 +1100

    All the Propeller mods in a single commit

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -450,6 +450,7 @@ gulp.task("generateDocumentation-watch", function () {
 gulp.task(
   "release",
   gulp.series(
+    "prepare",
     "build",
     "build-ts",
     combine,
@@ -1667,7 +1668,7 @@ function createTypeScriptDefinitions() {
 
   // Wrap the source to actually be inside of a declared cesium module
   // and add any workaround and private utility types.
-  source = `declare module "cesium" {
+  source = `declare module "@propelleraero/cesium" {
 ${source}
 }
 
@@ -1684,7 +1685,7 @@ ${source}
     const assignmentName = path.basename(file, path.extname(file));
     if (publicModules.has(assignmentName)) {
       publicModules.delete(assignmentName);
-      source += `declare module "cesium/Source/${moduleId}" { import { ${assignmentName} } from 'cesium'; export default ${assignmentName}; }\n`;
+      source += `declare module "@propelleraero/cesium/Source/${moduleId}" { import { ${assignmentName} } from '@propelleraero/cesium'; export default ${assignmentName}; }\n`;
     }
   });
 
