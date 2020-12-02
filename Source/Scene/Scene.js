b63762c1732a37e6405752113d61e03a1472c865
Chris Cooper <chris@propelleraero.com>
2020-12-02 14:20:50 +1100

    All the Propeller mods in a single commit
    Updated secrets (#18)
    
    Co-authored-by: Keat <keat@propelleraero.com.au>

--- Source/Scene/Scene.js
+++ Source/Scene/Scene.js
@@ -1878,6 +1878,8 @@ Scene.prototype.updateFrameState = function () {
   );
   frameState.occluder = getOccluder(this);
   frameState.minimumTerrainHeight = 0.0;
+  // PROPELLER HACK
+  frameState.minimumTerrainHeight = -500.0;
   frameState.minimumDisableDepthTestDistance = this._minimumDisableDepthTestDistance;
   frameState.invertClassification = this.invertClassification;
   frameState.useLogDepth =
