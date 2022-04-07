e2b7bc895139ae278aac372cfc39f3209be1fd04
Chris Cooper <chris@propelleraero.com.au>
2022-04-07 11:31:34 +1000

    revert defer change due to https://github.com/CesiumGS/cesium/pull/10272

--- Source/Core/defer.js
+++ Source/Core/defer.js
@@ -1,31 +1,30 @@
 /**
  * A function used to resolve a promise upon completion .
- * @callback resolve
+ * @callback defer.resolve
  *
  * @param {*} value The resulting value.
  */
 
 /**
  * A function used to reject a promise upon failure.
- * @callback reject
+ * @callback defer.reject
  *
  * @param {*} error The error.
  */
 
 /**
  * An object which contains a promise object, and functions to resolve or reject the promise.
- * @typedef {Object} deferred
- * @property {resolve} resolve Resolves the promise when called.
- * @property {reject} reject Rejects the promise when called.
- * @property {Promise<any>} promise Promise object.
+ *
+ * @typedef {Object} defer.deferred
+ * @property {defer.resolve} resolve Resolves the promise when called.
+ * @property {defer.reject} reject Rejects the promise when called.
+ * @property {Promise} promise Promise object.
  */
 
 /**
  * Creates a deferred object, containing a promise object, and functions to resolve or reject the promise.
- *
- * @function
- *
- * @returns {deferred}
+ * @returns {defer.deferred}
+ * @private
  */
 function defer() {
   let resolve;
