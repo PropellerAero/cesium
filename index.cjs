631d6f598ad158944ba209686c6549062fa3a3aa
katrin.schmid <katrin.schmid@propelleraer.com.au>
2023-01-16 15:16:36 +1100

    remove ppropellerhack

--- index.cjs
+++ index.cjs
@@ -3,21 +3,15 @@
 
 const path = require("path");
 
-const flavour = ""
+let flavour = "CesiumUnminified";
+
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
 if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging" ||
   process.env.NODE_ENV === "dev"
 ) {
-  // eslint-disable-next-line global-require
-  module.exports = require(path.join(__dirname, "Build/Cesium/index.cjs"));
-} else{
-  // eslint-disable-next-line global-require
-  module.exports = require(path.join(
-    __dirname,
-    "Build/CesiumUnminified/index.cjs"
-  ));
+  flavour = "Cesium";
 }
-
-module.exports = require(path.join(__dirname, "Build/Cesium/index.cjs"));
+const p = path.join(__dirname, "Build", flavour, "index.cjs");
+module.exports = require(p);
