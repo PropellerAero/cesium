5d2b9dac192dfaaa5a1b668743b2552aac1100a9
katrin.schmid <katrin.schmid@propelleraer.com.au>
2023-01-16 14:11:19 +1100

    no return

--- index.cjs
+++ index.cjs
@@ -3,6 +3,7 @@
 
 const path = require("path");
 
+const flavour = ""
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
 if (
   process.env.NODE_ENV === "production" ||
@@ -11,10 +12,12 @@ if (
 ) {
   // eslint-disable-next-line global-require
   module.exports = require(path.join(__dirname, "Build/Cesium/index.cjs"));
-  return;
+} else{
+  // eslint-disable-next-line global-require
+  module.exports = require(path.join(
+    __dirname,
+    "Build/CesiumUnminified/index.cjs"
+  ));
 }
 
-module.exports = require(path.join(
-  __dirname,
-  "Build/CesiumUnminified/index.cjs"
-));
+module.exports = require(path.join(__dirname, "Build/Cesium/index.cjs"));
