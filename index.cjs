3295a07e0b91bc9fe1cf4cd7ea548e2002f7252b
katrin.schmid <katrin.schmid@propelleraer.com.au>
2023-01-17 09:16:24 +1100

    flightpath

--- index.cjs
+++ index.cjs
@@ -3,15 +3,14 @@
 
 const path = require("path");
 
-let flavour = "CesiumUnminified";
-
 // If in 'production' mode, use the combined/minified/optimized version of Cesium
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging" ||
-  process.env.NODE_ENV === "dev"
-) {
-  flavour = "Cesium";
+if (process.env.NODE_ENV === "production") {
+  // eslint-disable-next-line global-require
+  module.exports = require(path.join(__dirname, "Build/Cesium/index.cjs"));
+  return;
 }
-const p = path.join(__dirname, "Build", flavour, "index.cjs");
-module.exports = require(p);
+
+module.exports = require(path.join(
+  __dirname,
+  "Build/CesiumUnminified/index.cjs"
+));
